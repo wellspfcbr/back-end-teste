e81c6dc02b88218703086ca7eceb9dcdcd832733
WESLLEY SANTOS SILVA <weslley.ssilva5@senacsp.edu.br>
2022-10-05 09:17:42 -0300

    first commit

---
+++ src/filmes.ts
@@ -0,0 +1,6 @@
+let filmes = ['Velozes e furiosos','A era do Gelo','Chuck','Todo mundo em Pânico','O massacre da serra elétrica']
+
+
+export default filmes
+
+
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Matematica from './matematica'
+
+import Filmes from './filmes'
+
+let n1: number = 10
+let n2: number = 20
+
+console.log("soma:"+Matematica.somar(n1,n2))
+
+console.log("subtrair: "+Matematica.subtrair(n1,n2))
+
+console.log("multiplicar: "+Matematica.multiplicar(n1,n2))
+
+
+console.log(Filmes)
