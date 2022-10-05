96deadc7afbed34a5f7d4b96ad3c72605d8c44de
WESLLEY SANTOS SILVA <weslley.ssilva5@senacsp.edu.br>
2022-10-05 11:45:34 -0300

    servidor criado

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/*
 import Matematica from './matematica'
 
 import Filmes from './filmes'
@@ -12,4 +13,27 @@ console.log("subtrair: "+Matematica.subtrair(n1,n2))
 console.log("multiplicar: "+Matematica.multiplicar(n1,n2))
 
 
-console.log(Filmes)
+console.log(Filmes)*/
+
+
+//importando express
+import  express  from 'express'
+import { Server } from 'http'
+import { fileURLToPath } from 'url'
+
+
+//usando express
+const server = express()
+
+server.listen(3000)
+//rota estatica
+server.get('/tratamentos',(req,res) =>{
+    res.send ('Pagina de tratamentos')
+})
+//rota dinamica
+server.get ('/passagens/:origem-:destino',(req,res) =>{
+    let origem : string =req.params.origem
+    let destino : string =req.params.destino
+
+    res.send(`passagem de ${origem} com destino até ${destino}`)
+})
